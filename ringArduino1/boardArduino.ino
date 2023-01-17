6ce6e6f34a267e5d0d6feaf89893338695d58ac8
Brendan Wilhelm <bwilhelm@andrew.cmu.edu>
2023-01-17 18:49:58 -0500

    Catan hardcoding and polling functions

--- ringArduino1/boardArduino.ino
+++ ringArduino1/boardArduino.ino
@@ -1,94 +1,245 @@
-/*
- 
-#include <SoftwareSerial.h>
 #include <Arduino.h>
 
+#define REEDSW_X0 11
+#define REEDSW_X1 12
+#define REEDSW_X2 13
+#define RDSW_X_NUM 3
 
-#define DESERT 0
-#define ROCK 1
-#define CLAY 2
-#define WHEAT 3
-#define WOOD 4
-#define SHEEP 5
+#define REEDSW_Y0 5
+#define REEDSW_Y1 6
+#define REEDSW_Y2 7 
+#define REEDSW_Y3 8
+#define REEDSW_Y4 9
+#define REEDSW_Y5 10
+#define RDSW_Y_NUM 6
 
-// Serial
-#define rxPin 10
-#define txPin 11
 
-// Set up a new SoftwareSerial object
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+#define VERTICES_X0 54
+#define VERTICES_X1 55
+#define VERTICES_X2 56
+#define VERTICES_X3 57
+#define VERTICES_X4 58
+#define VERTICES_X5 59 
+#define VERTICES_X6 60
+#define VERTICES_X7 61
+#define VERTICES_X8 62
+#define VERTICES_X_NUM 9
 
-/*
-void place(int tile_type, int *tile_indices, tile_count) {
-    for (int i = 0; i < tile_count; i++) {
-        switch(tile_type) {
-            case DESERT: break;
-            case ROCK: break;
-            case CLAY: break;
-            case WHEAT: break;
-            case WOOD: break;
-            case SHEEP: break;
-            default:
-        }
-    }
+#define VERTICES_Y0 63
+#define VERTICES_Y1 64
+#define VERTICES_Y2 65
+#define VERTICES_Y3 66
+#define VERTICES_Y4 66
+#define VERTICES_Y5 67
+#define VERTICES_Y_NUM 6
 
-}
+
+#define EDGES_X0 30
+#define EDGES_X1 32
+#define EDGES_X2 34
+#define EDGES_X3 36
+#define EDGES_X4 38
+#define EDGES_X5 40
+#define EDGES_X6 42
+#define EDGES_X7 44
+#define EDGES_X8 46
+#define EDGES_X9 48
+#define EDGES_X10 50
+#define EDGES_X11 52
+#define EDGES_X_NUM 12
+
+#define EDGES_Y0 43
+#define EDGES_Y1 45
+#define EDGES_Y2 47
+#define EGDES_Y3 49
+#define EDGES_Y4 51
+#define EDGES_Y5 53
+#define EDGES_Y_NUM 6
+
+
+#define DiceTX 16
+#define DiceRX 17
+
+#define ArduinoRingTX 18
+#define ArduinoRingRX 19
+
+#define piDisplayTX 14
+#define piDisplayRX 15
+
+using namespace std;
+map<pair<int, int>, pair<int, int>> button_ind_to_tileVertex_ind;
+map<pair<int, int>, pair<int, int>> button_ind_to_tileEdge_ind;
+map<pair<int, int>, pair<int, int>>::iterator iter;
+
+button_ind_to_tileVertex_ind[make_pair(0, 2)] = make_pair(2, 3);
+
+typedef enum {
+    desert = 0,
+    rock,
+    clay,
+    wheat,
+    wood,
+    sheep
+} resource_t;
+
+typedef enum {
+    none = 0,
+    blue_settlement,
+    blue_city,
+    green_settlement,
+    green_city,
+    red_settlement,
+    red_city,
+    orange_settlement,
+    orange_city
+} building_t;
+
+typedef struct vertex {
+    building_t type;
+    int ledID;
+} vertex_t;
+
+typedef struct edge {
+    building_t type;
+    int ledID1;
+    int ledID2;
+} edge_t;
+
+typedef struct tile_comp {
+    vertex_t[6] vertices;
+    edge_t[6] edges;
+    resource_t type;
+    int num;
+} tile_t;
+
+typedef struct catan_board {
+    tile_t[19] tiles;
+} board_t;
 
 
 void setup() {
-    // Setup for Software Serial
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
-    mySerial.begin(9600);
+    Serial.begin(9600);
+
+    pinMode(REEDSW_X0, OUTPUT);
+    pinMode(REEDSW_X1, OUTPUT);
+    pinMode(REEDSW_X2, OUTPUT);
+
+    pinMode(REEDSW_Y0, INPUT_PULLUP);
+    pinMode(REEDSW_Y1, INPUT_PULLUP);
+    pinMode(REEDSW_Y2, INPUT_PULLUP);
+    pinMode(REEDSW_Y3, INPUT_PULLUP);
+    pinMode(REEDSW_Y4, INPUT_PULLUP);
+    pinMode(REEDSW_X5, INPUT_PULLUP);
+    pinMode(REEDSW_X6, INPUT_PULLUP);
+
+    pinMode(VERTICES_X0, OUTPUT);
+    pinMode(VERTICES_X1, OUTPUT);
+    pinMode(VERTICES_X2, OUTPUT);
+    pinMode(VERTICES_X3, OUTPUT);
+    pinMode(VERTICES_X4, OUTPUT);
+    pinMode(VERTICES_X5, OUTPUT);
+    pinMode(VERTICES_X6, OUTPUT);
+    pinMode(VERTICES_X7, OUTPUT);
+    pinMode(VERTICES_X8, OUTPUT);
+
+    pinMode(VERTICES_Y0, INPUT_PULLUP);
+    pinMode(VERTICES_Y1, INPUT_PULLUP);
+    pinMode(VERTICES_Y2, INPUT_PULLUP);
+    pinMode(VERTICES_Y3, INPUT_PULLUP);
+    pinMode(VERTICES_Y4, INPUT_PULLUP);
+    pinMode(VERTICES_Y5, INPUT_PULLUP);
+
+    pinMode(EDGES_X0, OUTPUT);
+    pinMode(EDGES_X1, OUTPUT);
+    pinMode(EDGES_X2, OUTPUT);
+    pinMode(EDGES_X3, OUTPUT);
+    pinMode(EDGES_X4, OUTPUT);
+    pinMode(EDGES_X5, OUTPUT);
+    pinMode(EDGES_X6, OUTPUT);
+    pinMode(EDGES_X7, OUTPUT);
+    pinMode(EDGES_X8, OUTPUT);
+    pinMode(EDGES_X9, OUTPUT);
+    pinMode(EDGES_X10, OUTPUT);
+    pinMode(EDGES_X11, OUTPUT);
+
+    pinMode(EDGES_Y0, INPUT_PULLUP);
+    pinMode(EDGES_Y1, INPUT_PULLUP);
+    pinMode(EDGES_Y2, INPUT_PULLUP);
+    pinMode(EDGES_Y3, INPUT_PULLUP);
+    pinMode(EDGES_Y4, INPUT_PULLUP);
+    pinMode(EDGES_X5, INPUT_PULLUP);
+}
+       
+
+int pollReedSwitch() {
+    digitalWrite(REEDSW_X0, HIGH);
+    digitalWrite(REEDSW_X1, HIGH);
+    digitalWrite(REEDSW_X2, HIGH);
+
+    for (int i = REEDSW_X0; i < REEDSW_X2 + 1; i++) {
+        digitalWrite(i, LOW);
+        for(int j = REEDSW_Y0; i < REEDSW_Y5 + 1; j++) { 
+            // We are assigning the tile number based on its location in the reed switch matrix
+            if (digitalRead(j) == LOW) return (i+1)*(j+1);
+        }
+        digitalWrite(i, HIGH);
+    }
+    // The robber was not found so it's in the middle
+    return 18;
+}
+
+
+pair<int, int> pollEdges() {
+    digitalWrite(EDGES_X0, HIGH);
+    digitalWrite(EDGES_X1, HIGH);
+    digitalWrite(EDGES_X2, HIGH);
+    digitalWrite(EDGES_X3, HIGH);
+    digitalWrite(EDGES_X4, HIGH);
+    digitalWrite(EDGES_X5, HIGH);
+    digitalWrite(EDGES_X6, HIGH);
+    digitalWrite(EDGES_X7, HIGH);
+    digitalWrite(EDGES_X8, HIGH);
+    digitalWrite(EDGES_X9, HIGH);
+    digitalWrite(EDGES_X10, HIGH);
+    digitalWrite(EDGES_X11, HIGH); 
+    
+    for (int i = EDGES_X0; i < EDGES_X11 + 1; i += 2) {
+        digitalWrite(i, LOW);
+        for(int j = EDGES_Y0; j < EDGES_Y5 + 1; j += 2) { 
+            // We are assigning the tile number based on its location in the reed switch matrix
+            if (digitalRead(j) == 0) return button_ind_to_tileEdge_ind[make_pair(i, j)].second;
+        }
+        digitalWrite(i, HIGH);
+    }
+    
+    return make_pair(-1, -1);
+}
+
+pair<int, int> pollVertices() {
+    digitalWrite(VERTICES_X0, HIGH);
+    digitalWrite(VERTICES_X1, HIGH);
+    digitalWrite(VERTICES_X2, HIGH);
+    digitalWrite(VERTICES_X3, HIGH);
+    digitalWrite(VERTICES_X4, HIGH);
+    digitalWrite(VERTICES_X5, HIGH);
+    digitalWrite(VERTICES_X6, HIGH);
+    digitalWrite(VERTICES_X7, HIGH);
+    digitalWrite(VERTICES_X8, HIGH);
+    digitalWrite(VERTICES_X9, HIGH);
+    digitalWrite(VERTICES_X10, HIGH);
+    digitalWrite(VERTICES_X11, HIGH); 
+    
+    for (int i = VERTICES_X0; i < VERTICES_X8 + 1; i++) {
+        digitalWrite(i, LOW);
+        for(int j = VERTICES_Y0; j < VERTICES_Y5 + 1; j++) { 
+            // We are assigning the tile number based on its location in the reed switch matrix
+            if (digitalRead(j) == 0) return button_ind_to_tileEdge_ind[make_pair(i, j)].second;
+        }
+        digitalWrite(i, HIGH);
+    }
+    
+    return make_pair(-1, -1);
 }
 
 void loop() {
-    char[19] tiles;
-    int desert_ind = -1;
-    int sheep_ind = 0;
-    int rock_ind = 0;
-    int clay_ind = 0;
-    int wood_ind = 0;
-    int wheat_ind = 0;
-    int[3] rock_indices;
-    int[3] clay_indices;
-    int[4] wheat_indices;
-    int[4] wood_indices;
-    int[4] sheep_indices;
-
-    // Message parsing and sending
-    if (mySerial.available() > 0) {
-        if (mySerial.read() == 's') {
-
-            // Stream starts with an s and ends with an e
-            for(int i = 0; i < 19; i++) {
-                tiles[i] = mySerial.read();
-            }
-
-            // Read the end of message e
-            mySerial.read();
-        }
-
-        // Start tile placing sequence
-        // Place desert
-        for (int i = 0; i < 19; i++) {
-            switch(tiles[i]) {
-                case DESERT: desert_ind = i; break;
-                case ROCK: rock_indices[rock_ind] = i; rock_ind++; break;
-                case CLAY: clay_indices[clay_ind] = i; clay_ind++; break;
-                case WHEAT: wheat_indices[wheat_ind] = i; wheat_ind++; break;
-                case WOOD: wood_indices[wood_ind] = i; wood_ind++; break;
-                case SHEEP: sheep_indices[sheep_ind] = i; sheep_ind++; break;
-                default:
-            }
-        }
-
-        place(DESERT, &desert_ind, 1);
-        place(ROCK, rock_indices, rock_ind);
-        place(CLAY, clay_indices, clay_ind);
-        place(WHEAT, wheat_indices, wheat_ind);
-        place(WOOD, wood_indices, wood_ind);
-        place(SHEEP, sheep_indices, sheep_ind);
-    }
 }
-*/
--- ringArduino1/ringArduino1.ino
+++ ringArduino1/ringArduino1.ino
@@ -68,7 +68,7 @@ void setup() {
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   mySerial.begin(9600);
-  
+
   pinMode(ENC_SW, INPUT);
 
   woodCount = 0;
@@ -89,17 +89,13 @@ void setup() {
   RingArduinoCode = 5;
 
   //
-  // if (mySerial.available() > 0) {
-  //   RingArduinoCode = mySerial.read();
-  //   mySerial.write(RingArduinoCode + 1);
-  //}
+  //  RingArduinoCode = mySerial.read();
+  //  mySerial.write(RingArduinoCode + 1);
 
 }
 
-//long oldPosition  = -999;
-
 void loop() {
-  
+
   size_t newPosition = myEnc.read();
   if (newPosition != oldPosition) {
     oldPosition = newPosition;
@@ -107,7 +103,7 @@ void loop() {
   }
 
 
-  
+
 
   lc.setDigit(0, LEDSIZE - 1, 5, (newPosition/STEPSIZE) % 5 == 0);
   lc.setDigit(0, LEDSIZE - 2, sheepCount, (newPosition/STEPSIZE) % 5 == 1);
@@ -115,224 +111,51 @@ void loop() {
   lc.setDigit(0, LEDSIZE - 4, wheatCount, (newPosition/STEPSIZE) % 5 == 3);
   lc.setDigit(0, LEDSIZE - 5, rockCount, (newPosition/STEPSIZE) % 5 == 4);
 
-  
+  int messageLengthCount = 0;
 
+  // Message parsing and sending
+  if (mySerial.available() > 0) {
+      if (mySerial.read() == 's') {
 
-    int messageLengthCount = 0;
+          // Stream starts with an s and ends with an e
+          mySerial.write('s');
 
-    // Message parsing and sending
-    if (mySerial.available() > 0) {
-        if (mySerial.read() == 's') {
+          char currRead;
+          while ((currRead = mySerial.read()) != 'e') {
 
-            // Stream starts with an s and ends with an e
-            mySerial.write('s');
+              // Each Arduino has a seperate Code after whose 7 bytes
+              // are data values relevant to it
 
-            char currRead;
-            while ((currRead = mySerial.read()) != 'e') {
+              if (currRead == RingArduinoCode) {
+                  mySerial.write(RingArduinoCode);
 
-                // Each Arduino has a seperate Code after whose 7 bytes
-                // are data values relevant to it
+                  // Should we use this as a way to reset the rotary state? not hard
+                  mySerial.read();
+                  mySerial.write(encoderRotaryState);
 
-                if (currRead == RingArduinoCode) {
-                    mySerial.write(RingArduinoCode);
+                  woodCount += mySerial.read();
+                  mySerial.write(woodCount);
 
-                    // Should we use this as a way to reset the rotary state? not hard
-                    mySerial.read();
-                    mySerial.write(encoderRotaryState);
-                    
-                    woodCount += mySerial.read(); 
-                    mySerial.write(woodCount);
+                  sheepCount += mySerial.read();
+                  mySerial.write(sheepCount);
 
-                    sheepCount += mySerial.read();
-                    mySerial.write(sheepCount);
+                  clayCount += mySerial.read();
+                  mySerial.write(clayCount);
 
-                    clayCount += mySerial.read();
-                    mySerial.write(clayCount);
+                  wheatCount += mySerial.read();
+                  mySerial.write(wheatCount);
 
-                    wheatCount += mySerial.read();
-                    mySerial.write(wheatCount);
+                  rockCount += mySerial.read();
+                  mySerial.write(rockCount);
+              }
+              else {
+                  mySerial.write(currRead);
+              }
+          }
 
-                    rockCount += mySerial.read();
-                    mySerial.write(rockCount);
-                }
-                else {
-                    mySerial.write(currRead);
-                }
-            }
-
-            mySerial.write('e');
-        }
-    }
- 
-}
-
-
-/*
-
-#include <SoftwareSerial.h>
-#include <LedControl.h>
-#include <Arduino.h>
-#include "Encoder.h"
-
-
-// Serial
-#define rxPin 10
-#define txPin 11
-
-// 7-segment Control
-#define DataIn 5
-#define Clk 7
-#define CS 6
-#define ChainLength 1
-#define LEDSIZE 5
-
-#define ROCK 1
-#define CLAY 2
-#define WHEAT 3
-#define WOOD 4
-#define SHEEP 5
-
-#define DOWN 0
-#define UP 1
-#define switchPin 8
-
-#define MAXPOS 19
-#define MINPOS 0
-#define STEPSIZE 4
-
-// Ring Arduino Specific
-char RingArduinoCode;
-
-// Change these two numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
-Encoder myEnc(5, 6);
-//   avoid using pins with LEDs attached
-
-// Set up a new SoftwareSerial object
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
-
-// Define pins for LedControl. These can be any digital pins on microcontroller.
-LedControl lc = LedControl(DataIn, Clk, CS, ChainLength);
-
-char woodCount;
-char sheepCount;
-char clayCount;
-char wheatCount;
-char rockCount;
-
-char encoderSwitchState;
-char encoderRotaryState;
-char sevenSegVal;
-long oldPosition  = -999;
-
-void setup()  {
-
-    // Setup for Software Serial
-    Serial.begin(9600);
-    Serial.println("Boasic Encoder Test:");
-    /*
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
-    mySerial.begin(9600);
-
-    Serial.begin(9600);
-
-    pinMode(switchPin, INPUT);
-
-    woodCount = 0;
-    sheepCount = 0;
-    clayCount = 0;
-    wheatCount = 0;
-    rockCount = 0;
-
-    // Setup for 7-segments
-
-    // Initialize all displays
-    for (int dsp = 0; dsp < lc.getDeviceCount(); dsp++) {
-        lc.shutdown(dsp, false); // Wakeup display
-        lc.setIntensity(dsp, 2); // Set brightness to a lower level (1-15)
-        lc.clearDisplay(dsp);   // Clear the display
-    }
-
-
-    Serial.println("Basic Encoder Test:");
-
-    
-
-
-   
-
-	// Dynamically set the unique arduino code
-	//RingArduinoCode = mySerial.read();
-	//mySerial.write(RingArduinoCode + 1);
-}
-
-void loop() {
-
-  long newPosition = myEnc.read();
-  if (newPosition != oldPosition) {
-    oldPosition = newPosition;
-    Serial.println(newPosition);
+          mySerial.write('e');
+      }
   }
-    
-  /*
-    lc.setDigit(0, LEDSIZE - 1, woodCount, newPosition/STEPSIZE == 0);
-    lc.setDigit(0, LEDSIZE - 2, sheepCount, newPosition/STEPSIZE == 1);
-    lc.setDigit(0, LEDSIZE - 3, clayCount, newPosition/STEPSIZE == 2);
-    lc.setDigit(0, LEDSIZE - 4, wheatCount, newPosition/STEPSIZE == 3);
-    lc.setDigit(0, LEDSIZE - 5, rockCount, newPosition/STEPSIZE == 4);
 
-    int messageLengthCount = 0;
-
-    // Message parsing and sending
-    if (mySerial.available() > 0) {
-        if (mySerial.read() == 's') {
-
-            // Stream starts with an s and ends with an e
-            mySerial.write('s');
-
-            char currRead;
-            while ((currRead = mySerial.read()) != 'e') {
-
-                // Each Arduino has a seperate Code after whose 7 bytes
-                // are data values relevant to it
-
-                if (currRead == RingArduinoCode) {
-                    mySerial.write(RingArduinoCode);
-
-                    // Should we use this as a way to reset the rotary state? not hard
-                    mySerial.read();
-                    mySerial.write(encoderRotaryState);
-                    
-                    woodCount += mySerial.read(); 
-                    mySerial.write(woodCount);
-
-                    sheepCount += mySerial.read();
-                    mySerial.write(sheepCount);
-
-                    clayCount += mySerial.read();
-                    mySerial.write(clayCount);
-
-                    wheatCount += mySerial.read();
-                    mySerial.write(wheatCount);
-
-                    rockCount += mySerial.read();
-                    mySerial.write(rockCount);
-                }
-                else {
-                    mySerial.write(currRead);
-                }
-            }
-
-            mySerial.write('e');
-        }
-    }
 }
 
-*/
-
-
-// https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
-// http://www.mathertel.de/Arduino/RotaryEncoderLibrary.aspx
